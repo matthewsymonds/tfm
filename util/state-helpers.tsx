3a2ef89075aa6e17190341ceaaca004bc5637f1d
Billy Littlefield <billylittlefield@gmail.com>
2020-04-11 17:56:58 -0700

    fix the build

--- util/state-helpers.tsx
+++ util/state-helpers.tsx
@@ -1,24 +1,25 @@
 import {GameState} from '../reducer';
 import {Card} from '../constants/card-types';
+import {Resource} from '../constants/resource';
 
 const stateHelpers = {
-    canCardBePlayed(
-        state: GameState,
-        card: Card
-    ): {result: false; reasons: string} | {result: true} {
-        // todo -- account for steel/titanium/discounts
-        if (state.players[state.common.currentPlayerIndex].resources.Megacredit < card.cost) {
-            return {
-                result: false,
-                reasons: 'Cannot afford to play this card',
-            };
-        }
+    // canCardBePlayed(
+    //     state: GameState,
+    //     card: Card
+    // ): {result: false; reasons: string} | {result: true} {
+    //     // todo -- account for steel/titanium/discounts
+    //     if (state.players[state.common.currentPlayerIndex].resources[Resource.MEGACREDIT] < card.cost) {
+    //         return {
+    //             result: false,
+    //             reasons: 'Cannot afford to play this card',
+    //         };
+    //     }
 
-        const canCardBePlayed = card.canBePlayed ? card.canBePlayed(state) : true;
-        return canCardBePlayed
-            ? {result: true}
-            : {result: false, reasons: card.requirementFailedMessage};
-    }
+    //     const canCardBePlayed = card.canBePlayed ? card.canBePlayed(state) : true;
+    //     return canCardBePlayed
+    //         ? {result: true}
+    //         : {result: false, reasons: card.requirementFailedMessage};
+    // }
 };
 
 export default stateHelpers;
