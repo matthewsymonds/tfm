2febb29b12f4b6b585d1fd2e98257acf98600d64
Matt Symonds <matt.symonds@thetileapp.com>
2020-04-25 13:15:20 -0700

    get rid of tile placements filter

--- reducer.tsx
+++ reducer.tsx
@@ -169,9 +169,7 @@ function getInitialState(): GameState {
 
     const allCorporations = possibleCards.filter(card => card.type === CardType.CORPORATION);
 
-    const deck = possibleCards.filter(
-        card => card.type !== CardType.CORPORATION && card.tilePlacements.length > 0
-    );
+    const deck = possibleCards.filter(card => card.type !== CardType.CORPORATION);
     const possibleCorporations = sampleCards(allCorporations, 2);
     const startingCards = sampleCards(deck, 10);
 
