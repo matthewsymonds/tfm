f6635a897a2385d9fb26ef3c524b32bc0aab533a
Matthew Symonds <matthew.symonds@berkeley.edu>
2021-07-13 16:11:57 -0700

    ensure you get notified for your turn while drafting

--- pages/api/your-turn.tsx
+++ pages/api/your-turn.tsx
@@ -1,4 +1,6 @@
+import {GameStage} from 'constants/game';
 import {gamesModel, retrieveSession} from 'database';
+import {current} from 'immer';
 
 export default async (req, res) => {
     const sessionResult = await retrieveSession(req, res);
@@ -15,16 +17,61 @@ export default async (req, res) => {
             try {
                 games = await gamesModel.find(
                     {
-                        // Games with more than one player (player index 1 exists)
-                        'players.1': {$exists: true},
-                        // Where the current player has this username.
-                        currentPlayer: sessionResult.username,
+                        $and: [
+                            // Games with more than one player (player index 1 exists)
+                            {'players.1': {$exists: true}},
+                            {
+                                $or: [
+                                    {
+                                        // Where the current player has this username.
+                                        currentPlayer: sessionResult.username,
+                                    },
+                                    {
+                                        // or we'rd in draft/buy or discard
+                                        'state.players': {
+                                            $elemMatch: {
+                                                username: sessionResult.username,
+                                                pendingCardSelection: {
+                                                    $ne: null,
+                                                },
+                                            },
+                                        },
+                                        'state.common.gameStage': {
+                                            $in: [GameStage.BUY_OR_DISCARD, GameStage.DRAFTING],
+                                        },
+                                    },
+                                ],
+                            },
+                            {'state.common.gameStage': {$not: {$eq: GameStage.END_OF_GAME}}},
+                        ],
                     },
-                    'name'
+                    {name: 1, state: 1}
                 );
             } catch (error) {
                 games = [];
             }
+            // Unfortunately we have to do all this crap to detect when someone is waiting to draft...
+            games = games.filter(game => {
+                if (game.state.common.gameStage === GameStage.ACTIVE_ROUND) {
+                    return true;
+                }
+                const currentPlayer = game.state.players.find(
+                    player => player.username === sessionResult.username
+                );
+                const {pendingCardSelection} = currentPlayer;
+                if (!pendingCardSelection) return false;
+                if (game.state.common.gameStage === GameStage.BUY_OR_DISCARD) {
+                    return pendingCardSelection.possibleCards.length === 4;
+                }
+                if (game.state.common.gameStage === GameStage.DRAFTING) {
+                    return (
+                        pendingCardSelection.possibleCards.length +
+                            pendingCardSelection.draftPicks.length ===
+                        4
+                    );
+                }
+                return false;
+            });
             // Ensure the results are fresh!
             res.setHeader('cache-control', 'no-cache');
             return res.json({games});
