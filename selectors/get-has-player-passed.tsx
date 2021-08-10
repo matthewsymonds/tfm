0419e0c6b28bc9cd550761e4a0ed3d97c1407b6d
Matthew Symonds <matthew.symonds@berkeley.edu>
2021-08-10 13:10:07 -0700

    fix transient error

--- selectors/get-has-player-passed.tsx
+++ selectors/get-has-player-passed.tsx
@@ -2,5 +2,5 @@ import {GameState} from 'reducer';
 
 export function getHasPlayerPassed(playerIndex: number, state: GameState): boolean {
     const player = state.players[playerIndex];
-    return player.action === 0;
+    return player?.action === 0;
 }
