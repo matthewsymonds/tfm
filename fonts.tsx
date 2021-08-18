b5972a2fb7c9eceea3fbaf39d988994e559bc6b2
Matthew Symonds <matthew.symonds@berkeley.edu>
2021-08-18 10:52:24 -0700

    force font to preload properly

--- fonts.tsx
+++ fonts.tsx
@@ -4,7 +4,11 @@ export const Fonts = () => (
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
             href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed:wght@400;500;600;700;900&family=Open+Sans:wght@400;500;600;700;900"
-            rel="stylesheet"
+            rel="preload"
+            as="style"
+            onLoad={function () {
+                this.rel = 'stylesheet';
+            }}
         />
     </>
 );
