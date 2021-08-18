d41f4ac0b7702ef792323c64b2a05ad543633f96
Matthew Symonds <matthew.symonds@berkeley.edu>
2021-08-18 16:36:11 -0700

    suppress weird chrome no use font warning

--- fonts.tsx
+++ fonts.tsx
@@ -9,6 +9,7 @@ export const Fonts = () => (
             onLoad={function () {
                 this.rel = 'stylesheet';
             }}
+            crossOrigin="anonymous"
         />
     </>
 );
