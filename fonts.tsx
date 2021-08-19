5af353f6c40a5b8488e86bc15464112d6c2e6f66
Matthew Symonds <matthew.symonds@berkeley.edu>
2021-08-19 11:05:19 -0700

    fix fonts for mobile?

--- fonts.tsx
+++ fonts.tsx
@@ -5,10 +5,19 @@ export const Fonts = () => (
         <link
             href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed:wght@400;500;600;700;900&family=Open+Sans:wght@400;500;600;700;900"
             rel="preload"
+            id="preload-stylesheet"
             as="style"
             onLoad={function () {
-                this.rel = 'stylesheet';
+                const element = document.querySelector('#preload-stylesheet');
+                if (element && element instanceof HTMLLinkElement) {
+                    element.rel = 'stylesheet';
+                }
             }}
+            crossOrigin="anonymous"
+        />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed:wght@400;500;600;700;900&family=Open+Sans:wght@400;500;600;700;900"
+            rel="stylesheet"
             crossOrigin="anonymous"
         />
     </>
