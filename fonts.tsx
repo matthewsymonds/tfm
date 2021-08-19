2cc993b6fadee069d3f7431de3c39293091e9dc9
Matthew Symonds <matthew.symonds@berkeley.edu>
2021-08-19 12:18:10 -0700

    double rel for fallback

--- fonts.tsx
+++ fonts.tsx
@@ -1,27 +1,11 @@
-import {useEffect, useRef} from 'react';
-
-function preloadSupported() {
-    if (typeof document === 'undefined') return false;
-    var relListEl = document.createElement('link');
-    const relList = relListEl?.relList;
-    return !!(relList && relList.supports && relList.supports('preload'));
-}
-
 export const Fonts = () => {
-    const ref = useRef<HTMLLinkElement>(null);
-    useEffect(() => {
-        if (!preloadSupported() && ref.current) {
-            ref.current.rel = 'stylesheet';
-        }
-    }, [ref.current]);
     return (
         <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
             <link
-                ref={ref}
                 href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed:wght@400;500;600;700;900&family=Open+Sans:wght@400;500;600;700;900"
-                rel="preload"
+                rel="stylesheet preload"
                 id="preload-stylesheet"
                 as="style"
                 onLoad={function () {
