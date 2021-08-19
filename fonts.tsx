f89a8c90e5d56e6c0e40b81ce1b3d8b732a11a67
Matthew Symonds <matthew.symonds@berkeley.edu>
2021-08-19 11:53:57 -0700

    a bit more font tweaking

--- fonts.tsx
+++ fonts.tsx
@@ -1,24 +1,34 @@
+function preloadSupported() {
+    if (typeof document === 'undefined') return false;
+    var relListEl = document.createElement('link');
+    const relList = relListEl?.relList;
+    return !!(relList && relList.supports && relList.supports('preload'));
+}
+
 export const Fonts = () => (
     <>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-            href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed:wght@400;500;600;700;900&family=Open+Sans:wght@400;500;600;700;900"
-            rel="preload"
-            id="preload-stylesheet"
-            as="style"
-            onLoad={function () {
-                const element = document.querySelector('#preload-stylesheet');
-                if (element && element instanceof HTMLLinkElement) {
-                    element.rel = 'stylesheet';
-                }
-            }}
-            crossOrigin="anonymous"
-        />
-        <link
-            href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed:wght@400;500;600;700;900&family=Open+Sans:wght@400;500;600;700;900"
-            rel="stylesheet"
-            crossOrigin="anonymous"
-        />
+        {preloadSupported() ? (
+            <link
+                href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed:wght@400;500;600;700;900&family=Open+Sans:wght@400;500;600;700;900"
+                rel="preload"
+                id="preload-stylesheet"
+                as="style"
+                onLoad={function () {
+                    const element = document.querySelector('#preload-stylesheet');
+                    if (element && element instanceof HTMLLinkElement) {
+                        element.rel = 'stylesheet';
+                    }
+                }}
+                crossOrigin="anonymous"
+            />
+        ) : (
+            <link
+                href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed:wght@400;500;600;700;900&family=Open+Sans:wght@400;500;600;700;900"
+                rel="stylesheet"
+                crossOrigin="anonymous"
+            />
+        )}
     </>
 );
