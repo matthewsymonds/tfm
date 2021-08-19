9a807afc7720eb1116793c3ff51f2324da63a6ec
Matthew Symonds <matthew.symonds@berkeley.edu>
2021-08-19 12:28:50 -0700

    simplify and cleanup (first local test on mobile)

--- client-server-shared/get-path.tsx
+++ client-server-shared/get-path.tsx
@@ -1,4 +1,7 @@
 import {PROTOCOL_HOST_DELIMITER} from 'pages/_app';
+function isCharNumber(c) {
+    return c >= '0' && c <= '9';
+}
 
 export function getPath(path, isServer, req, headers) {
     let url: string;
@@ -12,6 +15,6 @@ export function getPath(path, isServer, req, headers) {
     }
 
     const {host} = headers;
-    const protocol = /^localhost(:\d+)?$/.test(host) ? 'http' : 'https';
+    const protocol = /^localhost(:\d+)?$/.test(host) || isCharNumber(host[0]) ? 'http' : 'https';
     return protocol + PROTOCOL_HOST_DELIMITER + host + path;
 }
--- fonts.tsx
+++ fonts.tsx
@@ -5,15 +5,8 @@ export const Fonts = () => {
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
             <link
                 href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed:wght@400;500;600;700;900&family=Open+Sans:wght@400;500;600;700;900"
-                rel="stylesheet preload"
-                id="preload-stylesheet"
+                rel="preload stylesheet"
                 as="style"
-                onLoad={function () {
-                    const element = document.querySelector('#preload-stylesheet');
-                    if (element && element instanceof HTMLLinkElement) {
-                        element.rel = 'stylesheet';
-                    }
-                }}
                 crossOrigin="anonymous"
             />
         </>
