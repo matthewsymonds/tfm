5a52400b253bf73da03eb1b935592508f30e2673
Matthew Symonds <matthew.symonds@berkeley.edu>
2021-08-19 20:16:54 -0700

    hard code list

--- pages/api/cron-job.tsx
+++ pages/api/cron-job.tsx
@@ -11,7 +11,7 @@ const rawFiles = glob.sync(ROOT_FOLDER + '/**/*');
 
 const BASE_URL = 'https://tfm-henna.vercel.app';
 
-const files = rawFiles
+let files = rawFiles
     .filter(dir => dir.endsWith(SUFFIX))
     .map(dir => dir.slice(PREFIX.length).replace('[id]', '1').replace('[name]', 1))
     .map(dir => dir.slice(0, dir.length - SUFFIX.length))
@@ -21,6 +21,24 @@ const files = rawFiles
 const FOUR_MINUTES = 60 * 1000 * 4;
 
 let timeoutId;
+
+// override what we generate locally (can't be generated in production sadly):
+
+files = [
+    'https://tfm-henna.vercel.app/api/cron-job',
+    'https://tfm-henna.vercel.app/api/games',
+    'https://tfm-henna.vercel.app/api/games/1',
+    'https://tfm-henna.vercel.app/api/games/1/play',
+    'https://tfm-henna.vercel.app/api/sessions',
+    'https://tfm-henna.vercel.app/api/users',
+    'https://tfm-henna.vercel.app/api/your-turn',
+    'https://tfm-henna.vercel.app/games/1',
+    'https://tfm-henna.vercel.app/index',
+    'https://tfm-henna.vercel.app/login',
+    'https://tfm-henna.vercel.app/logout',
+    'https://tfm-henna.vercel.app/new-game',
+    'https://tfm-henna.vercel.app/signup',
+];
 
 runCronJob();
 
@@ -38,5 +56,5 @@ async function runCronJob() {
 }
 
 export default async (req, res) => {
-    res.json({files});
+    res.json({});
 };
